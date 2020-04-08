c618d983c91d013890efa2ea02a604f5702b98c6
KareemDabbour <kareem.dabbour41@gmail.com>
2020-04-08 17:21:50 -0400

    hopefully works

---
+++ deleteProp-Host.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="home1.css">
+</head>
+<body>
+<?php
+	session_start();
+	$conn_string = $_SESSION['connString'];
+	$dbconn = pg_connect($conn_string) or die('Connection failed');
+
+	$prop_id = array_keys($_POST)[0];
+
+	$q = "delete from Property where property_id= $prop_id";
+	$result = pg_query($dbconn,$q);
+	$q1 = "delete from pricing_agreement where property_id = $prop_id";
+	$res1 = pg_query($dbconn, $q1);
+	$q2 = "delete from booking where property_id = $prop_id";
+	$res2 = pg_query($dbconn, $q2);
+	header("Location: ./employee_view_properties.php");
+	exit();
+?>
+</body>
+</html>
--- viewproperty.php
+++ viewproperty.php
@@ -16,8 +16,16 @@
                       WHERE p.user_id = '$uid'";
                 $stmt = pg_prepare($dbconn,"pt",$q);
                 $result = pg_query($dbconn,$q);
-                echo "<table id='resultable'>
+                echo "
+                <col>
+                <col>
+                <colgroup span = '7'></colgroup>
+                <FORM method = 'POST' action = '/deleteProp-Host.php' id = 'form1'>
+                <table id='resultable'>
                 <thead>
+                    <th>
+                        Image
+                    </th>
                     <th id='resultheader'>
                         Address
                     </th>
@@ -34,27 +42,38 @@
                         # of Bathooms
                     </th>
                     <th id='resultheader'>
-                        Description
-                    </th>
-                    <th id='resultheader'>
                         Delete
                     </th>
                 </thead>
                 <tbody>"
                 ;
                 while($row = pg_fetch_row($result)){
+
                     echo "<tr>";
+                    $im = "SELECT image FROM property_image WHERE  property_id = ${row[0]}";
+                    $res = pg_query($dbconn,$im);
+                    if (pg_affected_rows($res) == 0){
+                        echo 
+                        "<tr>
+                          <td id='resultimg'><img class = 'zoom' src = '/images/default.jpeg' heigt = '84' width = '84'/>";
+                    }else{
+                        echo 
+                        "<tr>
+                              <td id='resultimg'><img class = 'zoom' src = '/images/" . pg_fetch_result($res, 0)    .
+                        "' heigt = '84' width = '84'/></td>";
+                    }
                     echo "
                     <td id='resultdata'>"        . $row[1] . 
                     "</td><td id='resultdata'>" . $row[2] . 
                     "</td><td id='resultdata'>" . $row[3] . 
                     "</td><td id='resultdata'>" . $row[5] . 
                     "</td><td id='resultdata'>" . $row[4] .
-                    "</td><td id='resultdata'>" . $row[6] .
-                    "</td><td id='resultdata'> <button type = \"button\" id = 1>Delete</button></td>"; 
+                    "<tfoot><tr> <br></tr><td colspan = '1' scope = 'colgroup'> Description:</td> 
+                    <td colspan = '6' scope ='colgroup'>".$row[6]."</td></tfoot>"
+                    "</td><td><input type = 'submit' name = '${row[0]}' value= 'Delete'></td>"; 
                     echo "</tr>";
                 }
-                echo "</tbody></table>";
+                echo "</tbody></table></FORM>";
             ?>
             <br>
         </div>
